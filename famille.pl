660757e2117cf6e916d90f921a9a8c71fa055ea9
chlaubry <caubry@enssat.fr>
2026-02-26 16:14:50 +0100

    Ajout de la règle fratrie dans famille

--- famille.pl
+++ famille.pl
@@ -21,9 +21,13 @@ parent(jeanpaul,francois).
 parent(sandrine,hugo).
 parent(stephane,hugo).
 parent(francois,chloe).
-parent(francois,jean).
 parent(nadine,chloe).
+parent(francois,jean).
 parent(nadine,jean).
+parent(nadine,mathieu).
+parent(francois,mathieu).
+parent(nadine,veronique).
+parent(francois,veronique).
 
 % REGLES
 
@@ -32,3 +36,7 @@ mere(X,Y) :- femme(X), parent(X,Y).
 
 % Pere
 pere(X,Y) :- homme(X), parent(X,Y).
+
+% Fratrie
+
+fratrie(X,Y) :- mere(Z,X), mere(Z,Y), X\=Y.
